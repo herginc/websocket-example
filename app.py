baad1b894238ede405785d05d7644042da14cc03
herginc <82322244+herginc@users.noreply.github.com>
2025-10-08 18:26:39 +0800

    Bugfix: Use sid argument in SocketIO event handlers instead of request.sid

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import time
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# 儲存已連線的 client sid
 connected_clients = set()
 
 @app.route('/')
@@ -16,22 +15,21 @@ def index():
     return "WebSocket Server is running!"
 
 @socketio.on('connect')
-def handle_connect():
-    print('Client connected:', request.sid)
-    connected_clients.add(request.sid)
+def handle_connect(sid, environ):
+    print('Client connected:', sid)
+    connected_clients.add(sid)
 
 @socketio.on('disconnect')
-def handle_disconnect():
-    print('Client disconnected:', request.sid)
-    connected_clients.discard(request.sid)
+def handle_disconnect(sid):
+    print('Client disconnected:', sid)
+    connected_clients.discard(sid)
 
 def send_periodic_message():
     while True:
         for sid in list(connected_clients):
             socketio.emit('server_message', {'data': '定時訊息：' + time.strftime('%Y-%m-%d %H:%M:%S')}, to=sid)
-        socketio.sleep(60)  # 每 60 秒送一次
+        socketio.sleep(60)
 
-# 啟動 background task
 socketio.start_background_task(send_periodic_message)
 
 if __name__ == '__main__':
